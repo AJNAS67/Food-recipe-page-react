52d424dd4d178c59661246fdea99b78172c102f8
ajnaskp67 <ajnaskp67@gmail.com>
2024-03-13 20:26:40 +0530

    details page and favore page

--- src/context/index.jsx
+++ src/context/index.jsx
@@ -1,4 +1,5 @@
 import { useState, createContext } from "react";
+import { useNavigate } from "react-router-dom";
 
 export const GlobalContext = createContext(null);
 
@@ -7,6 +8,9 @@ export default function GlobalState({ children }) {
   const [searchParam, setSearchParam] = useState("");
   const [recipeList, setRecipeList] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [favoriteList, setFavoriteList] = useState([]);
+  const navigate = useNavigate()
+
 
   console.log(searchParam);
 
@@ -20,19 +24,42 @@ export default function GlobalState({ children }) {
       );
       const data = await response.json();
       setRecipeList(data?.data?.recipes);
-      setSearchParam('')
+      setSearchParam("");
       setLoading(false);
       console.log(data?.data?.recipes);
+      navigate('/')
     } catch (error) {
       console.log(error);
       setLoading(false);
-      setSearchParam('')
+      setSearchParam("");
 
     }
   }
+  async function handleAddToFavorite(event) {
+    let copFavoriteList = [...favoriteList];
+    const index=copFavoriteList.findIndex((item)=>item.id===event.id)
+   
+    if(index===-1){
+      copFavoriteList.push(event)
+    }
+    else{
+      copFavoriteList.splice(index)
+      console.log(copFavoriteList,'copFavoriteListcopFavoriteListcopFavoriteList');
+
+    }
+    setFavoriteList(copFavoriteList)
+  }
   return (
     <GlobalContext.Provider
-      value={{ searchParam, setSearchParam, handleSubmit ,loading,recipeList}}
+      value={{
+        searchParam,
+        setSearchParam,
+        handleSubmit,
+        loading,
+        recipeList,
+        handleAddToFavorite,
+        favoriteList
+      }}
     >
       {children}
     </GlobalContext.Provider>
--- src/pages/favorate/index.jsx
+++ src/pages/favorate/index.jsx
@@ -1,4 +1,21 @@
-export default function Favorites(){
-    return(<div>Favorate components</div>)
+import { useContext } from "react";
+import { GlobalContext } from "../../context";
+import RecipeItem from "../../components/recipe-item";
 
-} 
+export default function Favorites() {
+  const { favoriteList } = useContext(GlobalContext);
+  console.log(favoriteList, "favoriteList");
+  return (
+    <div className="py-8 container mx-auto flex flex-wrap justify-center gap-10">
+      {favoriteList && favoriteList.length > 0 ? (
+        favoriteList.map((item) => <RecipeItem item={item} />)
+      ) : (
+        <div>
+          <p className="lg:text-4xl text-xl text-center text-black font-extrabold">
+            Nothing is added in favorites.
+          </p>
+        </div>
+      )}
+    </div>
+  );
+}
