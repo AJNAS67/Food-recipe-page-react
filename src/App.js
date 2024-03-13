3db372b7ad9025901e94ac4662c658928b836dc4
ajnaskp67 <ajnaskp67@gmail.com>
2024-03-13 20:26:25 +0530

    details pafe and favorite page added

--- src/App.js
+++ src/App.js
@@ -4,16 +4,17 @@ import NavBar from "./components";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/home";
 import Favorites from "./pages/favorate";
+import Details from "./pages/details";
 
 function App() {
   return (
-    <div >
+    <div>
       <div className="min-h-screen p-6 bg-white text-gray-600 text-lg">
         <NavBar />
         <Routes>
-          <Route path="/" element={<Home/>}></Route>
-          <Route path="/favorites" element={<Favorites/>}></Route>
-
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/favorites" element={<Favorites />}></Route>
+          <Route path="/recipe-item/:id" element={<Details />}></Route>
         </Routes>
       </div>
     </div>
